c47ab63e214fd6cbbe2e7e32af5375066892efbc
Wolfgang Schnerring <wosc@wosc.de>
2010-06-29 14:19:50 +0200

    implemented badic entry point and configuration mechanism

--- src/gocept/amqprun/tests/test_server.py
+++ src/gocept/amqprun/tests/test_server.py
@@ -9,45 +9,45 @@ import unittest
 
 class MessageReaderTest(unittest.TestCase):
 
+    hostname = 'localhost'
     queue_name = 'test'
 
     def setUp(self):
-        self.connection = amqp.Connection(host='localhost')
+        self.connection = amqp.Connection(host=self.hostname)
         self.channel = self.connection.channel()
         self.channel.queue_purge(self.queue_name)
 
     def tearDown(self):
+        self.reader.stop()
+        self.thread.join()
+
         self.channel.queue_purge(self.queue_name)
         self.channel.close()
         self.connection.close()
 
-    def test_loop_can_be_stopped_from_outside(self):
+    def _create_reader(self):
         import gocept.amqprun.server
-        reader = gocept.amqprun.server.MessageReader()
-        t = threading.Thread(target=reader.start)
-        t.start()
+        self.reader = gocept.amqprun.server.MessageReader(self.hostname)
+        self.thread = threading.Thread(target=self.reader.start)
+        self.thread.start()
+
+    def test_loop_can_be_stopped_from_outside(self):
+        # this test simply should not hang indefinitely
+        self._create_reader()
         time.sleep(0.1)
-        reader.stop()
-        t.join()
 
     def test_messages_arrive_in_task_queue(self):
-        import gocept.amqprun.server
-        reader = gocept.amqprun.server.MessageReader()
-        t = threading.Thread(target=reader.start)
-        t.start()
+        self._create_reader()
 
         self.send_message('foo')
-        self.assertEqual(1, reader.tasks.qsize())
-        message = reader.tasks.get()
+        self.assertEqual(1, self.reader.tasks.qsize())
+        message = self.reader.tasks.get()
         self.assertEqual('foo', message.body)
 
         self.send_message('bar')
-        self.assertEqual(1, reader.tasks.qsize())
-        message = reader.tasks.get()
+        self.assertEqual(1, self.reader.tasks.qsize())
+        message = self.reader.tasks.get()
         self.assertEqual('bar', message.body)
-
-        reader.stop()
-        t.join()
 
     def send_message(self, body):
         self.channel.basic_publish(amqp.Message(body), 'amq.fanout')
