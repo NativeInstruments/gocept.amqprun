94fa7b9096c0cb80523bcf0674638b8f204c8100
Wolfgang Schnerring <wosc@wosc.de>
2010-06-29 11:24:37 +0200

    initial skeleton

---
+++ setup.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='gocept.amqprun',
+    version='0.1dev',
+    author='gocept',
+    author_email='mail@gocept.com',
+    url='',
+    description="""\
+""",
+    long_description= (
+        open('README.txt').read()
+        + '\n\n'
+        + open('CHANGES.txt').read()),
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data = True,
+    zip_safe=False,
+    license='ZPL',
+    namespace_packages = ['gocept'],
+    install_requires=[
+        'pika',
+        'setuptools',
+    ],
+    extras_require=dict(test=[
+        'zope.testing',
+    ]),
+)
---
+++ src/gocept/amqprun/interfaces.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import zope.interface
