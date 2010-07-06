b75a2959bd644d6d90832f5abdefb866d6e977e3
Wolfgang Schnerring <ws@gocept.com>
2010-07-06 10:53:33 +0200

    helper scripts for send/receive

--- send.py
+++ send.py
@@ -1,6 +1,7 @@
 import amqplib.client_0_8 as amqp
 import sys
 
-connection = amqp.Connection(host='localhost')
-channel = connection.channel()
-channel.basic_publish(amqp.Message(sys.argv[1]), 'amq.fanout')
+with amqp.Connection(host='localhost') as connection:
+    with connection.channel() as channel:
+        channel.basic_publish(
+            amqp.Message(sys.argv[2]), 'amq.topic', routing_key=sys.argv[1])
