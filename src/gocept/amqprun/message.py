17cbb848f1d7d6e9275351a28fcc852092d0e760
Wolfgang Schnerring <wosc@wosc.de>
2010-07-27 11:11:41 +0200

    extracted Message into its own module

---
+++ src/gocept/amqprun/message.py
@@ -0,0 +1,38 @@
+# Copyright (c) 2010 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import gocept.amqprun.interfaces
+import pika.spec
+import time
+import zope.interface
+
+
+class Message(object):
+
+    zope.interface.implements(gocept.amqprun.interfaces.IMessage)
+
+    exchange = 'amq.topic'
+
+    def __init__(self, header, body, delivery_tag=None, routing_key=None):
+        if not isinstance(header, pika.spec.BasicProperties):
+            header = self.convert_header(header)
+        self.header = header
+        self.body = body
+        self.delivery_tag = delivery_tag
+        self.routing_key = (
+            unicode(routing_key).encode('UTF-8') if routing_key else None)
+        gocept.amqprun.interfaces.IMessage.validateInvariants(self)
+
+    def convert_header(self, header):
+        header = header.copy()
+        result = pika.spec.BasicProperties()
+        result.timestamp = time.time()
+        result.delivery_mode = 2 # persistent
+        for key in dir(result):
+            value = header.pop(key, self)
+            if isinstance(value, unicode):
+                value = value.encode('UTF-8')
+            if value is not self:
+                setattr(result, key, value)
+        result.headers = header
+        return result
---
+++ src/gocept/amqprun/tests/test_message.py
@@ -0,0 +1,37 @@
+import unittest
+import zope.interface.verify
+import time
+
+
+class TestMessage(unittest.TestCase):
+
+    def test_message_should_provide_IMessage(self):
+        from gocept.amqprun.message import Message
+        import gocept.amqprun.interfaces
+        message = Message({}, 2, 3)
+        zope.interface.verify.verifyObject(
+            gocept.amqprun.interfaces.IMessage,
+            message)
+        self.assertEqual(2, message.body)
+        self.assertEqual('amq.topic', message.exchange)
+
+    def test_header_should_be_converted_to_BasicProperties(self):
+        from gocept.amqprun.message import Message
+        import pika.spec
+        now = int(time.time())
+        message = Message(
+            dict(foo='bar', content_type='text/xml', app_id='myapp'), None,
+            delivery_tag=1)
+        header = message.header
+        self.assertTrue(isinstance(header, pika.spec.BasicProperties))
+        self.assertEqual(dict(foo='bar'), header.headers)
+        self.assertTrue(header.timestamp >= now)
+        self.assertEqual('text/xml', header.content_type)
+        self.assertEqual(2, header.delivery_mode) # persistent
+        self.assertEqual('myapp', header.app_id)
+
+    def test_routing_key_and_delivery_tag_should_be_exclusive(self):
+        from gocept.amqprun.message import Message
+        self.assertRaises(
+            zope.interface.Invalid,
+            Message, {}, None, delivery_tag=1, routing_key=2)
