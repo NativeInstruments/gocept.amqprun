e43214f354b5de1c7af35fd88bcef59ce6836541
Christian Zagrodnick <cz@flyingcircus.io>
2010-08-13 12:50:57 +0200

    Preparing release 0.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.amqprun',
-    version='0.1dev',
+    version='0.1',
     author='gocept',
     author_email='mail@gocept.com',
     url='',
