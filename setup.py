2e3ecc275d8bd9d3698c465a2473bccc1e17d8a0
Christian Zagrodnick <cz@flyingcircus.io>
2010-08-13 12:51:16 +0200

    Back to development: 0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.amqprun',
-    version='0.1',
+    version='0.2dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='',
