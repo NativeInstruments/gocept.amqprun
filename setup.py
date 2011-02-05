fe2fd8fb3c44a708ba8bbf336fcbab732c89baad
Christian Zagrodnick <cz@gocept.com>
2011-02-05 16:19:03 +0100

    Preparing release 0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.amqprun',
-    version='0.2',
+    version='0.3',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/gocept-amqprun',
