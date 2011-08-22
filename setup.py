c0c66d2cb0daab2c1a9f10cda3fd4ace6eb329e3
Wolfgang Schnerring <wosc@wosc.de>
2011-08-22 11:14:12 +0200

    Back to development: 0.4.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.amqprun',
-    version='0.4.1',
+    version='0.4.2dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='https://intra.gocept.com/projects/projects/gocept-amqprun',
