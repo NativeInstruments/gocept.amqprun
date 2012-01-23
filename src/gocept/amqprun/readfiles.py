3cd3ceff5214bafc2d69d3b2ccd3d6886bd0d9a9
Wolfgang Schnerring <ws@gocept.com>
2012-01-23 11:53:39 +0100

    Re #10177: Implement ZCML directive to set up FileStoreReader thread

--- src/gocept/amqprun/readfiles.py
+++ src/gocept/amqprun/readfiles.py
@@ -4,38 +4,46 @@
 import gocept.amqprun.interfaces
 import gocept.filestore
 import logging
+import threading
 import time
+import transaction
 import zope.component
+import zope.configuration.fields
 import zope.interface
+import zope.schema
 
 
 log = logging.getLogger(__name__)
 
 
-class FileStoreReader(object):
-
-    zope.interface.implements(gocept.amqprun.interfaces.ILoop)
+class FileStoreReader(threading.Thread):
 
     def __init__(self, path, routing_key):
         self.running = False
         self.routing_key = routing_key
         self.filestore = gocept.filestore.FileStore(path)
         self.filestore.prepare()
+        super(FileStoreReader, self).__init__()
+        self.daemon = True
 
-    def start(self):
+    def run(self):
+        log.info('starting')
         self.running = True
         while self.running:
             self.scan()
             time.sleep(1)
 
     def stop(self):
+        log.info('stopping')
         self.running = False
+        self.join()
 
     def scan(self):
         for filename in self.filestore.list('new'):
-            log.debug('reading %r' % filename)
+            log.debug('sending %r to %r' % (filename, self.routing_key))
             self.send(open(filename).read())
             self.filestore.move(filename, 'new', 'cur')
+            transaction.commit()
 
     def send(self, body):
         # XXX make content-type configurable?
@@ -43,3 +51,24 @@ class FileStoreReader(object):
             {'content_type': 'text/xml'}, body, routing_key=self.routing_key)
         sender = zope.component.getUtility(gocept.amqprun.interfaces.ISender)
         sender.send(message)
+
+
+class IReadFilesDirective(zope.interface.Interface):
+
+    directory = zope.configuration.fields.Path(
+        title=u"Path to the directory from which to read files")
+
+    routing_key = zope.schema.TextLine(
+        title=u"Routing key to send to")
+
+
+def readfiles_directive(_context, directory, routing_key):
+    reader = FileStoreReader(directory, routing_key)
+    zope.component.zcml.subscriber(
+        _context,
+        for_=(gocept.amqprun.interfaces.IProcessStarting,),
+        handler=lambda event: reader.start())
+    zope.component.zcml.subscriber(
+        _context,
+        for_=(gocept.amqprun.interfaces.IProcessStopping,),
+        handler=lambda event: reader.stop())
