d9c42c2f660e124559ed871f21ff01f4b58a736a
Wolfgang Schnerring <wosc@wosc.de>
2013-05-28 10:51:41 +0200

    Properly clean up after ourselves

--- src/gocept/amqprun/tests/test_session.py
+++ src/gocept/amqprun/tests/test_session.py
@@ -4,6 +4,7 @@
 import mock
 import threading
 import time
+import transaction
 import unittest
 import zope.component
 import zope.component.testing
@@ -27,6 +28,7 @@ class DataManagerTest(unittest.TestCase):
         self.channel.connection_lock = self.connection.lock
 
     def tearDown(self):
+        transaction.abort()
         zope.component.testing.tearDown()
 
     def get_message(self):
