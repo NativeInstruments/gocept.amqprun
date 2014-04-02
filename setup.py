8fe29da7074877f38db76772afe7c28aa1e92319
Wolfgang Schnerring <wosc@wosc.de>
2014-04-02 08:57:34 +0200

    Include all extras in the test extra

--- setup.py
+++ setup.py
@@ -4,6 +4,40 @@
 from setuptools import setup, find_packages
 
 
+install_requires = [
+    'ZConfig',
+    'pika < 0.9',
+    'setuptools',
+    'transaction',
+    'zope.component[zcml]',
+    'zope.configuration',
+    'zope.event',
+    'zope.interface',
+    'zope.schema',
+]
+
+writefiles_require = [
+    'zope.xmlpickle',
+]
+
+readfiles_require = [
+    'gocept.filestore',
+]
+
+security_require = [
+    'zope.security>=4.0.0dev',
+]
+
+tests_require = writefiles_require + readfiles_require + security_require + [
+    'amqplib',
+    'gocept.testing',
+    'mock>=0.7.0',
+    'plone.testing',
+    'tcpwatch',
+    'zope.testing',
+]
+
+
 setup(
     name='gocept.amqprun',
     version='0.12.3.dev0',
@@ -34,35 +68,13 @@ Kit (ZTK) so you can use adapters, utilities and all the buzz.
     ],
     license='ZPL',
     namespace_packages=['gocept'],
-    install_requires=[
-        'ZConfig',
-        'pika < 0.9',
-        'setuptools',
-        'transaction',
-        'zope.component[zcml]',
-        'zope.configuration',
-        'zope.event',
-        'zope.interface',
-        'zope.schema',
-    ],
+    install_requires=install_requires,
     extras_require=dict(
-        test=[
-            'amqplib',
-            'gocept.testing',
-            'mock>=0.7.0',
-            'plone.testing',
-            'tcpwatch',
-            'zope.testing',
-            ],
-        writefiles=[
-            'zope.xmlpickle',
-            ],
-        readfiles=[
-            'gocept.filestore',
-            ],
-        security=[
-            'zope.security>=4.0.0dev',
-           ]),
+        test=tests_require,
+        writefiles=writefiles_require,
+        readfiles=readfiles_require,
+        security=security_require,
+    ),
     entry_points=dict(console_scripts=[
         'server = gocept.amqprun.main:main',
     ]),
