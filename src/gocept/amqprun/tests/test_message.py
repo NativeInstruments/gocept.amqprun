d08c696d3a386893a79fb7f11c40021cad25d498
Wolfgang Schnerring <wosc@wosc.de>
2014-06-02 14:53:53 +0200

    Add type check for message body (so it breaks immediately instead of during actual sending in pika)

--- src/gocept/amqprun/tests/test_message.py
+++ src/gocept/amqprun/tests/test_message.py
@@ -13,11 +13,11 @@ class TestMessage(unittest.TestCase):
     def test_message_should_provide_IMessage(self):
         from gocept.amqprun.message import Message
         import gocept.amqprun.interfaces
-        message = Message({}, 2, 3)
+        message = Message({}, 'body')
         zope.interface.verify.verifyObject(
             gocept.amqprun.interfaces.IMessage,
             message)
-        self.assertEqual(2, message.body)
+        self.assertEqual('body', message.body)
         self.assertEqual('amq.topic', message.exchange)
 
     def test_header_should_be_converted_to_BasicProperties(self):
@@ -32,7 +32,7 @@ class TestMessage(unittest.TestCase):
         self.assertEqual(dict(foo='bar'), header.headers)
         self.assertTrue(header.timestamp >= now)
         self.assertEqual('text/xml', header.content_type)
-        self.assertEqual(2, header.delivery_mode) # persistent
+        self.assertEqual(2, header.delivery_mode)  # persistent
         self.assertEqual('myapp', header.app_id)
 
     def test_converted_header_should_contain_message_id(self):
