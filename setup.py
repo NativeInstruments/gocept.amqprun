6a902b3f22c27208b7060ee15dc6c06dcd3c6d90
Wolfgang Schnerring <ws@gocept.com>
2014-09-09 15:20:58 +0200

    Preparing release 0.14.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_require = writefiles_require + readfiles_require + security_require + [
 
 setup(
     name='gocept.amqprun',
-    version='0.14.0.dev0',
+    version='0.14.0',
     author='gocept <cz at gocept dot com>',
     author_email='cz@gocept.com',
     url='https://code.gocept.com/hg/public/gocept.amqprun',
