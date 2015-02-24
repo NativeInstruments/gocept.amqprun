343a0843ebc515132a0eebdf96705d0387d54e5e
Michael Howitz <mh@gocept.com>
2015-02-24 10:53:11 +0100

    Preparing release 0.15.2

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_require = writefiles_require + readfiles_require + security_require + [
 
 setup(
     name='gocept.amqprun',
-    version='0.15.2.dev0',
+    version='0.15.2',
     author='gocept <cz at gocept dot com>',
     author_email='cz@gocept.com',
     url='https://bitbucket.org/gocept/gocept.amqprun',
