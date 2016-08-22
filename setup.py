ee418cffb0a8e5da65c5f723fb3a3023e9f9ab21
Michael Howitz <mh@gocept.com>
2016-08-22 11:33:17 +0200

    Back to development: 1.8

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ tests_require = (
 
 setup(
     name='gocept.amqprun',
-    version='1.7.1',
+    version='1.8.dev0',
     author='gocept <mail at gocept dot com>',
     author_email='mail@gocept.com',
     url='https://bitbucket.org/gocept/gocept.amqprun',
